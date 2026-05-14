08d7c229d2ad3c2c318359156629e11fff2544b5
KennySth <kenny_sthiven@outlook.com>
2026-05-13 21:46:26 -0500

    Update: Controlador de Login actualizado.

--- src/main/java/com/utp/meditrackapp/features/auth/ui/LoginController.java
+++ src/main/java/com/utp/meditrackapp/features/auth/ui/LoginController.java
@@ -2,6 +2,7 @@ package com.utp.meditrackapp.features.auth.ui;
 
 import com.utp.meditrackapp.core.config.NavigationService;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -9,7 +10,22 @@ import javafx.scene.control.Hyperlink;
 import org.kordamp.ikonli.javafx.FontIcon;
 import java.io.IOException;
 
+import javafx.scene.control.Alert;
+import com.utp.meditrackapp.features.auth.Dao.UsuarioDao;
+import com.utp.meditrackapp.core.models.entity.Usuario;
+import com.utp.meditrackapp.core.config.SessionManager;
+
 public class LoginController {
+    @FXML
+    private UsuarioDao usuarioDao;
+    public void Initialize(){
+        try{
+        usuarioDao = new UsuarioDao();
+        } catch (Exception e){
+            showAlert(Alert.AlertType.ERROR, "Error de Conexión", "No se pudo conectar a la base de datos.");
+            throw new RuntimeException("Error al inicializar UsuarioDao: " + e.getMessage());
+        }
+    }
 
     @FXML
     private TextField dniField;
@@ -52,7 +68,32 @@ public class LoginController {
 
     @FXML
     protected void onLogin() throws IOException {
-        // Redirección directa al Dashboard por ahora según lo solicitado
-        NavigationService.toDashboard();
+        String dni = dniField.getText();
+        String password = isPasswordVisible ? passwordTextField.getText() : passwordField.getText();
+
+        if(dni.isEmpty() || password.isEmpty()){
+            showAlert(Alert.AlertType.WARNING, "Campos Vacíos", "Por favor, ingrese sus credenciales.");
+            return;
+        }
+
+        Usuario usuario = usuarioDao.login(dni, password);
+        if (usuario != null) {
+            SessionManager.getInstance().login(usuario);
+            try {
+                NavigationService.toDashboard();
+            } catch (IOException e) {
+                showAlert(Alert.AlertType.ERROR, "Error de Navegación", "No se pudo cargar el panel de control.");
+            }
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Error de Autenticación", "Número de documento o contraseña incorrectos.");
+        }  
+    }
+
+    private void showAlert(Alert.AlertType type, String title, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
